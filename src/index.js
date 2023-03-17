a160c622c7a7f8afdf83124895a0ac4983001e9f
WFrog2511 <yadokugaeru@outlook.jp>
2023-03-17 22:24:52 +0900

    :sparkles: todoリストの削除メソッド

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Todo from './todo';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Todo />
   </React.StrictMode>
 );
 
--- src/todo.js
+++ src/todo.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Todo extends Component{
+export default class Todo extends Component{
   constructor(props) {
     super(props);
     this.state = {
@@ -22,6 +22,13 @@ class Todo extends Component{
     });
   }
 
+  removeTodo = (index) => {
+    const { todos, name } = this.state;
+    this.setState({
+      todos: [...todos.slice(0, index), ...todos.slice(index + 1)]
+    });
+  }
+
   render() {
     const { todos } = this.state;
 
@@ -29,7 +36,11 @@ class Todo extends Component{
       <input type="text" onInput={this.onInput} />
       <button onClick={this.addTodo} >登録</button>
       <ul>
-        {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+        {todos.map((todo, index) => <li key={index}>
+          {todo}
+          <button onClick={() => {this.removeTodo(index)}}>削除</button>
+        </li>)}
+
       </ul>
     </div>);
   }
